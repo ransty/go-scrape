d00eb3c06d93eacf31df0258d10c9f0324de6d94
Keano Porcaro <keano.porcaro@protonmail.com>
2018-12-14 11:23:02 +1100

    Added link scraper from golang tutorial

--- src/scrape.go
+++ src/scrape.go
@@ -4,29 +4,71 @@ import (
 	"net/http"
 	"time"
 	"log"
-	"os"
-	"io"
+	"fmt"
+	
+	"golang.org/x/net/html"
 )
 
 func main() {
-	// Create HTTP client with timeout
-	client := &http.Client{
+	links, err := Extract("https://keano.me")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(links)
+}
+
+func Extract(url string) ([]string, error) {
+	// Create HTTP client with Timeout
+	client := &http.Client {
 		Timeout: 30 * time.Second,
 	}
-
-	// HTTP request
-	response, err := client.Get("https://google.com")
+	
+	// HTTP request to the link
+	response, err := client.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer response.Body.Close()
-	
-	// Copy data from the response to standard output
-	n, err := io.Copy(os.Stdout, response.Body)
+
+	// Copy data from the response to be scraped for links
+	body, err := html.Parse(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("Number of bytes copied to STDOUT:", n)
+	// Now get the links within the body and search them with the forEachNode method, store in array of strings
+	var links []string
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+				link, err := response.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue
+				}
+				links = append(links, link.String())
+			}
+
+		}
+	}
+	forEachNode(body, visitNode, nil)
+	return links, nil
+}	
+
+
+// Basic traversal of nodes
+// See gopl.io/ch5/outline2
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
 }
 
