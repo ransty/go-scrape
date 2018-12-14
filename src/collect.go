ecef59b1fa7b6f2eef7b6a2d133788ca3ad51346
Keano Porcaro <keano.porcaro@protonmail.com>
2018-12-14 12:33:58 +1100

    Added web crawler and paragraph collector

---
+++ src/collect.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	// Instantiate default collector
+	c := colly.NewCollector(
+		colly.AllowedDomains("keano.me"),
+	)
+
+	c.OnHTML("p", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+	
+	c.Visit("https://keano.me")
+}
